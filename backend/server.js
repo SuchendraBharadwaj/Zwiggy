2b25f4012dddec0f593c7cc8a3ec3a73b15eeecc
SuchendraBharadwaj <101274701+SuchendraBharadwaj@users.noreply.github.com>
2024-06-14 19:59:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,12 @@ const port = process.env.PORT || 4000;
 
 // middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:[""],
+    methods : ["POST","GET"],
+    credentials: true
+    
+}));
 
 // db connection
 connectDB()
