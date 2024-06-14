c7b9dd22c7f4eb23c72ce114686441c3ffd7809d
SuchendraBharadwaj <101274701+SuchendraBharadwaj@users.noreply.github.com>
2024-06-15 00:42:40 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://zwiggybackend-suchendras-projects.vercel.app"
+    const url = "https://zwiggy-backend-0bor.onrender.com/"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
