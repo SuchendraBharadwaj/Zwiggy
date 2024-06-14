95d369288f9861a56538c57c36cb27ee7bf37162
SuchendraBharadwaj <101274701+SuchendraBharadwaj@users.noreply.github.com>
2024-06-14 20:35:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000;
 // middlewares
 app.use(express.json())
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://zwiggy-rho.vercel.app"],
     methods : ["POST","GET"],
     credentials: true
     
